2402aee28c1af5b6bb01867a7d3abb501b51f531
MOSBAHI Abdelaziz <mosbehiasiz@gmail.com>
2025-07-22 00:51:47 +0100

    update ACI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://asksphere-backend.azurewebsites.net/',
+  // apiUrl: 'https://asksphere-backend.azurewebsites.net/',
+  apiUrl: 'http://asksphere-backend.westus.azurecontainer.io:8000',
   aiApiUrl: 'https://<your-ai-backend-app>.azurewebsites.net/api'
 };
