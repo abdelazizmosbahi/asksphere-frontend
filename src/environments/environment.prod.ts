7242818ca41f87047c830b75432a148cc08a9bca
MOSBAHI Abdelaziz <mosbehiasiz@gmail.com>
2025-07-23 11:52:26 +0100

    update url backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://asksphere-backend.azurewebsites.net/',
-  apiUrl: 'http://asksphere-backend.westus.azurecontainer.io:8000',
+  apiUrl: 'http://asksphere-backend.westus.azurecontainer.io:5000',
   aiApiUrl: 'https://<your-ai-backend-app>.azurewebsites.net/api'
 };
